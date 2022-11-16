f150fcad30b493e505b7e78a70dc092afe3968a6
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2022-11-16 02:31:03 +0100

    dynamic gestures for spot

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from grlib.exceptions import NoHandDetectedException
 from grlib.load_data.by_folder_loader import ByFolderLoader
 from grlib.feature_extraction.pipeline import Pipeline
 from grlib.filter.false_positive_filter import FalsePositiveFilter
+from grlib.dynamic_detection import DynamicDetector
+from grlib.load_data.dynamic_gesture_loader import DynamicGestureLoader
+from grlib.trajectory.trajectory_classifier import TrajectoryClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
@@ -19,28 +22,8 @@ from PIL import Image
 import cv2.cv2 as cv
 
 
-def create_classifier(pipeline):
-    # Create, train and test a model for static gesture recognition
-    loader = ByFolderLoader(pipeline, path='./out')
-    loader.create_landmarks()
-
-    dataset = loader.load_landmarks()
-    X = dataset.iloc[:, :63]
-    y = dataset.iloc[:, 63]
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-
-    model = KNeighborsClassifier(5)
-    model.fit(X_train, y_train)
-
-    preds = model.predict(X_test)
-    print(preds, y_test)
-    print(accuracy_score(preds, y_test))
-
-    return model, dataset
-
+ZERO_PRECISION = 0.1
+NUM_HANDS = 1
 
 # TODO: Maybe figure out how exactly it is going to stand before we run it (so we don't break something)
 # TODO: The methods are synchronous now, maybe make them async somehow
@@ -71,7 +54,32 @@ def stop(command_client):
     command_client.robot_command(cmd)
 
 
-def recognize_gestures(robot, model, pipeline, fp_filter):
+def recognize_gestures(robot, pipeline):
+    loader = DynamicGestureLoader(
+        pipeline, 'left_right_dataset', trajectory_zero_precision=ZERO_PRECISION, key_frames=3
+    )
+    #loader.create_landmarks()
+
+    landmarks = loader.load_landmarks()
+    trajectories = loader.load_trajectories()
+    x_traj = trajectories.iloc[:, :-1]
+    y = np.array(trajectories['label'])
+
+    trajectory_classifier = TrajectoryClassifier()
+    trajectory_classifier.fit(np.array(x_traj), y)
+
+    start_shapes = loader.get_start_shape(landmarks, NUM_HANDS)
+
+    detector = DynamicDetector(
+        start_shapes,
+        y,
+        pipeline,
+        start_pos_confidence=0.1,
+        trajectory_classifier=trajectory_classifier,
+        update_candidates_every=5,
+        candidate_zero_precision=ZERO_PRECISION,
+    )
+
     # Initialize an ImageClient
     image_client = robot.ensure_client(ImageClient.default_service_name)
     sources = image_client.list_image_sources()
@@ -81,7 +89,6 @@ def recognize_gestures(robot, model, pipeline, fp_filter):
 
     while True:
         # Retrieve image from the arm camera
-        # TODO: Replace to match arm camera
         image_response = image_client.get_image_from_sources(['hand_color_image'])[0]
         image = np.array(Image.open(io.BytesIO(image_response.shot.image.data)))[:, :, ::-1]
 
@@ -90,27 +97,16 @@ def recognize_gestures(robot, model, pipeline, fp_filter):
             break
 
         # Process the image through the pipeline and run prediction
-        # TODO: maybe let get_world_landmarks_from_image already return flattened list so we dont have to do it here
         try:
-            landmarks = pipeline.get_world_landmarks_from_image(image)
-            pipeline.optimize()
-            prediction = model.predict(np.expand_dims(landmarks, axis=0))
-
-            # Check if gesture is valid and if so do the corresponding action
-            print(fp_filter.closest_representative(landmarks))
-            if fp_filter.is_relevant(landmarks):
-                if prediction == 'left':
-                    move_left(command_client)
-                elif prediction == 'right':
-                    move_right(command_client)
-                elif prediction == 'stop':
-                    stop(command_client)
-                elif prediction == 'up':
-                    stand_high(command_client)
-                elif prediction == 'down':
-                    stand_low(command_client)
+            detector.analyze_frame(image)
+            prediction = detector.last_pred
+            if prediction == 'left':
+                move_left(command_client)
+            elif prediction == 'right':
+                move_right(command_client)
             else:
-                print('Not valid')
+                stop(command_client)
+
         except NoHandDetectedException:
             print('No hand')
 
@@ -124,7 +120,6 @@ def main():
     # Construct the model for gesture recognition
     pipeline = Pipeline(num_hands=1)
     pipeline.add_stage(0, 0)
-    model, dataset = create_classifier(pipeline)
 
     # Set up the boston dynamics SPOT (based on hello_spot.py)
     sdk = bosdyn.client.create_standard_sdk('HelloSpotClient')
@@ -141,10 +136,8 @@ def main():
         robot.power_on(timeout_sec=20)
         assert robot.is_powered_on(), "Robot power on failed."
 
-        # Create false-positive filter to recognize only meaningful gestures
-        fp_filter = FalsePositiveFilter(dataset)
         # Start recognizing gestures
-        recognize_gestures(robot, model, pipeline, fp_filter)
+        recognize_gestures(robot, pipeline)
 
         robot.power_off(cut_immediately=False, timeout_sec=20)
         assert not robot.is_powered_on(), "Robot power off failed."
